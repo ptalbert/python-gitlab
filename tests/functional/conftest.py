8b043eb4ecbd67a84280a9d7353871ffb1c8037d
Patrick Talbert <ptalbert@redhat.com>
2025-12-18 17:26:13 +0100

    fix(tests): restore gitlab_plan and related fixtures
    Signed-off-by: Patrick Talbert <ptalbert@redhat.com>

---
+++ tests/functional/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+import gitlab
+from gitlab.testing.fixtures.helpers import get_gitlab_plan
+
+
+@pytest.fixture(scope="session")
+def gitlab_plan(gl: gitlab.Gitlab) -> str | None:
+    return get_gitlab_plan(gl)
+
+
+@pytest.fixture(autouse=True)
+def gitlab_premium(gitlab_plan, request) -> None:
+    if gitlab_plan in ("premium", "ultimate"):
+        return
+
+    if request.node.get_closest_marker("gitlab_premium"):
+        pytest.skip("Test requires GitLab Premium plan")
+
+
+@pytest.fixture(autouse=True)
+def gitlab_ultimate(gitlab_plan, request) -> None:
+    if gitlab_plan == "ultimate":
+        return
+
+    if request.node.get_closest_marker("gitlab_ultimate"):
+        pytest.skip("Test requires GitLab Ultimate plan")
